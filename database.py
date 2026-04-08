d10d309a0bc9580dca6b697af56a5cc6c7b15762
Sus6666 <susana.lara9947@alumnos.udg.mx>
2026-04-08 14:53:33 -0600

    Update database.pye

--- database.py
+++ database.py
@@ -1,1 +1,8 @@
+from sqlalchemy import create_engine, Column, Integer, String, Boolean, DateTime, Time, Date, ForeignKey, Text
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+from datetime import datetime, time, date
+
+Base = declarative_base()
+engine = create_engine('sqlute:///sigha.db' , echo= True)
 
