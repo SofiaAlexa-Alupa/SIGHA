daaf148a2032e3e2e489d8e9e0ae66350d4c41f6
cross <crossx230.game@outlook.com>
2026-04-26 13:01:03 -0600

    * Se quitaron los nombres de clases en mayusculas para una mejor legilibidad a la hora de importar *Se agregaron clas clases fecha, nombre y hora ya debuggueadas * La clase seccion y usuario se entregan de manera parcial, aun faltan caracteristicas, pero con los TODO * El archivo de pruebas es un eemplo de como usar las funciones dentro del codigo main

---
+++ main.py
@@ -0,0 +1,18 @@
+#Librerias utilizadas
+import flet as ft
+
+
+#Importacion de las clases
+from administrador import Administrador
+from alumno import Alumno
+from database import *
+from maestro import Maestro
+from materia import Materia
+from nombre import Nombre
+from seccion import Seccion
+from fecha import Fecha
+from hora import Hora
+
+def main(pagina: ft.Page):
+
+
