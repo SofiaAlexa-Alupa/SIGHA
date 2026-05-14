c22ad44b1a8ef7d3619faae191e89abc7e4c02b7
renatod4474-k <renato.diaz4474@alumnos.udg.mx>
2026-05-14 04:09:48 +0000

    mesi

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ import interfaz_grafica as interfaz
 from administrador import Administrador
 from alumno import Alumno
 from maestro import Maestro
+from materia import Materia
+
+# Base de datos temporales
+db_alumnos : list[Alumno]=[]
+db_maestros : list[Maestro]=[]
+db_materias : list[Materia]=[]
 
 def main(pagina: ft.Page):#
     pagina.tittle = "SIGHA"#Titulo de la pagina
@@ -20,11 +26,11 @@ def main(pagina: ft.Page):#
     usuario = Maestro()
     #usuario = Alumno()
 
-    if isinstance(usuario, Administrador):
-        pass
+     if isinstance(usuario, Administrador):
+         interfaz.interfaz_administrador(pagina,usuario,db_materias,db_alumnos,db_maestros)
 
-    if isinstance(usuario, Alumno):
-        interfaz.interfaz_alumno(pagina, usuario)
+     if isinstance(usuario, Alumno):
+        interfaz.interfaz_alumno(  pagina, usuario, db_materias)
 
     if isinstance(usuario, Maestro):
         interfaz.interfaz_maestro(pagina, usuario)
